760224f2fb59b0f34c91495653aef2786230deaf
Karsten Sperling Opdal <karsten.sperling.opdal@laerdal.com>
2026-01-14 11:22:09 +0100

    feat: Register WIC, VSI, and SPU file extensions on all platforms
    Add file type associations so users can open image files directly with
    the imager. Also accept SPU files as command-line arguments.
    
    - Windows: Add .vsi and .spu to OpenWithProgIds registry entries
    - macOS: Add CFBundleDocumentTypes and UTImportedTypeDeclarations
    - Linux: Add MIME types and update .desktop MimeType field

--- src/main.cpp
+++ src/main.cpp
@@ -461,7 +461,8 @@ int main(int argc, char *argv[])
                  firstPos.endsWith(".wic.gz", Qt::CaseInsensitive) ||
                  firstPos.endsWith(".wic.zst", Qt::CaseInsensitive) ||
                  firstPos.endsWith(".wic.bz2", Qt::CaseInsensitive) ||
-                 firstPos.endsWith(".vsi", Qt::CaseInsensitive))
+                 firstPos.endsWith(".vsi", Qt::CaseInsensitive) ||
+                 firstPos.endsWith(".spu", Qt::CaseInsensitive))
         {
             // Image file passed as argument - pre-select it in the GUI
             QFileInfo fi(firstPos);
@@ -475,7 +476,7 @@ int main(int argc, char *argv[])
         else
         {
             cerr << "Unknown positional argument ignored: " << firstPos << endl;
-            cerr << "Supported file types: .wic, .wic.xz, .wic.gz, .wic.zst, .wic.bz2, .vsi, ." MANIFEST_EXTENSION ", .json" << endl;
+            cerr << "Supported file types: .wic, .wic.xz, .wic.gz, .wic.zst, .wic.bz2, .vsi, .spu, ." MANIFEST_EXTENSION ", .json" << endl;
         }
     }
 
